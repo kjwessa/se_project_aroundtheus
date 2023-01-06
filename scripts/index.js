7e0051a79bd7494ed8fec9e6c7ec859d4f2e488c
Kevin Wessa <kjwessa@gmail.com>
2023-01-06 10:49:06 -0600

    Add profile.js for profile modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,18 +24,3 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
-
-const profileEditButton = document.querySelector(".profile__edit-button");
-const modal = document.querySelector(".modal");
-const modalCloseButton = document.querySelector(".modal__close-button");
-
-function openModalProfile() {
-  modal.classList.add("modal__opened");
-}
-profileEditButton.addEventListener("click", openModalProfile);
-
-function closeModalProfile() {
-  modal.classList.remove("modal__opened");
-}
-
-modalCloseButton.addEventListener("click", closeModalProfile);
