a974305fd235a4e0d53171e7d9c88f604a8e177b
Kevin Wessa <kjwessa@gmail.com>
2023-01-07 10:35:39 -0600

    Feat: Add JS to open and close modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,46 @@
+/* Variables for Profile */
+const profile = document.querySelector(".profile");
+const profileInfo = profile.querySelector(".profile__info");
+const profileTitle = profileInfo.querySelector(".profile__title");
+const profileSubTitle = profileInfo.querySelector(".profile__subtitle");
+const profileEditButton = profileInfo.querySelector(".profile__edit-button");
+
+/* Variables for Modal + Form */
+const profileModal = document.querySelector(".modal");
+const profileForm = profileModal.querySelector(".modal__container");
+const profileNameInput = profileModal.querySelector(".form__input-name");
+const profileJobInput = profileModal.querySelector(".form__input-job");
+const profileCloseButton = profileModal.querySelector(".modal__close-button");
+
+/* Open profile modal and fill form with values */
+function openModal() {
+  profileNameInput.value = profileTitle.textContent;
+  profileJobInput.value = profileSubTitle.textContent;
+  profileModal.classList.add("modal__opened");
+  console.log("Modal Opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+/* Close profile modal */
+function closeModal() {
+  profileModal.classList.remove("modal__opened");
+  console.log("Modal Closed");
+}
+
+profileCloseButton.addEventListener("click", closeModal);
+
+/* Form Submission - Prevent Default */
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameInput.value = profileTitle.textContent;
+  profileJobInput.value = profileSubTitle.textContent;
+  closeModal();
+}
+
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
+/* Initial Cards */
 const initialCards = [
   {
     name: "Yosemite Valley",
