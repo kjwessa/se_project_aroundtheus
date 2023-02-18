36dd92546594b67b01343978dc0f7539826f1b5d
Kevin Wessa <kjwessa@gmail.com>
2023-02-18 08:04:34 -0600

    feat: remove validation error when form valid

--- scripts/validation.js
+++ scripts/validation.js
@@ -5,7 +5,12 @@ function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   errorMessageEl.classList.add(errorClass);
 }
 
-function hideInputError() {}
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
 
 function checkInputValidity(formEl, inputEl, options) {
   if (!inputEl.validity.valid) {
