22cf9a395550ed19d22f0ad1f6bb6eecf12fb35d
Kevin Wessa <kjwessa@gmail.com>
2023-02-21 09:51:05 -0600

    feat: disable buttons until form is valid

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,59 +1,72 @@
-function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classList.add(inputErrorClass);
-  errorMessageEl.textContent = inputEl.validationMessage;
-  errorMessageEl.classList.add(errorClass);
+function showInputError(formElement, inputElement, { inputErrorClass, errorClass }) {
+  const errorMessageElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorMessageElement.textContent = inputElement.validationMessage;
+  inputElement.classList.add(errorClass);
 }
 
-function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classList.remove(inputErrorClass);
-  errorMessageEl.textContent = "";
-  errorMessageEl.classList.remove(errorClass);
+function hideInputError(formElement, inputElement, { inputErrorClass, errorClass }) {
+  const errorMessageElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorMessageElement.textContent = "";
+  inputElement.classList.remove(errorClass);
 }
 
-function checkInputValidity(formEl, inputEl, options) {
-  if (!inputEl.validity.valid) {
-    showInputError(formEl, inputEl, options);
+function checkInputValidity(formElement, inputElement, options) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, options);
   } else {
-    hideInputError(formEl, inputEl, options);
+    hideInputError(formElement, inputElement, options);
   }
 }
 
-function setEventListeners(formEl, options) {
-  // below, we destructure the variable we need from the options object
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputElement) => inputElement.validity.valid);
+}
+
+function disableButton(submitButton, inactiveButtonClass) {
+  submitButton.classList.add(inactiveButtonClass);
+  submitButton.disabled = true;
+}
+
+function enableButton(submitButton, inactiveButtonClass) {
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function toggleButtonState(inputElements, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputElements)) {
+    disableButton(submitButton, inactiveButtonClass);
+    return;
+  }
+
+  enableButton(submitButton, inactiveButtonClass);
+}
+
+function setEventListeners(formElement, options) {
   const { inputSelector } = options;
-  // but it could written also like this
-  // const inputSelector = options.inputSelector
-  const inputEls = [...formEl.querySelectorAll(inputSelector)];
-  // then we create an array from all the inputs on each form
-  inputEls.forEach((inputEl) => {
-    // and for each input
-    // we attach an event listener
-    inputEl.addEventListener("input", (e) => {
-      // check the right form
-      // check for the right input
-      // and we pass along the options so it has the classes
-      checkInputValidity(formEl, inputEl, options);
+  const inputElements = [...formElement.querySelectorAll(inputSelector)];
+  const submitButton = formElement.querySelector(options.submitButtonSelector);
+  inputElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", (evt) => {
+      checkInputValidity(formElement, inputElement, options);
+      toggleButtonState(inputElements, submitButton, options);
     });
   });
 }
 
 function enableValidation(options) {
-  // we grab all the forms on the page and put them in an array
-  const formEls = [...document.querySelectorAll(options.formSelector)];
-  // run a forEach over every form
-  formEls.forEach((formEl) => {
-    // attach an event listener and listen for the event
-    formEl.addEventListener("submit", (e) => {
-      // prevent the default form submission
-      e.preventDefault();
+  const { formSelector } = options;
+  const formElements = [...document.querySelectorAll(formSelector)];
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
     });
-    // then call the event listenrs and pass through each form along with the options
-    setEventListeners(formEl, options);
+    setEventListeners(formElement, options);
   });
 }
 
+// Object of Variables
 const configObject = {
   formSelector: ".modal__form-container",
   inputSelector: ".modal__form-input",
@@ -63,4 +76,5 @@ const configObject = {
   errorClass: "modal__error_visible",
 };
 
+// Validate the forms
 enableValidation(configObject);
