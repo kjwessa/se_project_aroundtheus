31cc48473ef21a2c2382499d91ee845d55254fd7
Kevin Wessa <kjwessa@gmail.com>
2023-02-22 08:49:49 -0600

    feat: modal close on click

--- scripts/index.js
+++ scripts/index.js
@@ -63,17 +63,25 @@ const cardTemplate = document.querySelector("#card-template").content;
 function openModal(modalName) {
   modalName.classList.add("modal_opened");
   document.addEventListener("keydown", closeModalOnEscape);
+  document.addEventListener("click", closeModalOnClick);
 }
 
 function closeModal(modalName) {
   modalName.classList.remove("modal_opened");
   document.removeEventListener("keydown", closeModalOnEscape);
+  document.removeEventListener("click", closeModalOnClick);
 }
 
 function closeModalOnEscape(evt) {
   if (evt.key === "Escape") {
     const openedModal = document.querySelector(".modal_opened");
     closeModal(openedModal);
+  }
+}
+
+function closeModalOnClick(evt) {
+  if (evt.target.classList.contains("modal_opened")) {
+    closeModal(evt.target);
   }
 }
 
