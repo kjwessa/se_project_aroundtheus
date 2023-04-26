97a716d00812acfe480f5680964613132938a18a
Kevin Wessa <kjwessa@gmail.com>
2023-04-26 09:32:57 -0500

    chores: remove high level comments from Section

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,24 +1,17 @@
 export default class Section {
   constructor({ items, renderer }, classSelector) {
-    // Store the items properties in the property _items
     this._items = items;
-
-    // Store the renderer property in the property _renderer
     this._renderer = renderer;
-
-    // Find and store the container element using classSelector in the property _container
     this._container = document.querySelector(classSelector);
   }
 
   renderItems() {
-    // For each item in _items, call the _renderer method with the item as an argument
     this._items.forEach((item) => {
       this._renderer(item);
     });
   }
 
   addItem(item) {
-    // Add the item to the beginning of the _container's children
     this._container.prepend(item);
   }
 }
