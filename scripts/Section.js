1731f3962ac75034ad54c502fcda03c2b6e7b5a0
Kevin Wessa <kjwessa@gmail.com>
2023-04-26 09:35:10 -0500

    chores: remove high-level comments from Section

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,8 +1,8 @@
 export default class Section {
-  constructor({ items, renderer }, classSelector) {
+  constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
-    this._container = document.querySelector(classSelector);
+    this._container = document.querySelector(containerSelector);
   }
 
   renderItems() {
