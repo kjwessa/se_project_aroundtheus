c9b6ea4e745938819005129837d1a66bfa5437ad
Kevin Wessa <kjwessa@gmail.com>
2023-04-27 07:53:15 -0500

    chores: add detailed comments to PopupWithImage

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -1,28 +1,26 @@
+// imports the Popup class from Popup.js.
 import Popup from "./Popup.js";
-
+// defines an exported PopupWithImage class that extends the Popup class.
 export default class PopupWithImage extends Popup {
+  // is the constructor method for the PopupWithImage class.
   constructor(popupSelector) {
-    // Call the constructor of the superclass (Popup) with an object containing the property popupSelector
+    //  calls the parent class Popup's constructor, passing in popupSelector.
     super({ popupSelector });
-
-    // Find and store the image element in the property _image
-    this._image = this._popupElement.querySelector(".modal__preview-image");
-
-    // Find and store the caption element in the property _caption
-    this._caption = this._popupElement.querySelector(".modal__preview-caption");
+    // gets a reference to the .modal__preview-image element within the popup.
+    this._previewModalImage = this._popupElement.querySelector(".modal__preview-image");
+    // gets a reference to the .modal__preview-caption element within the popup.
+    this._previewModalCaption = this._popupElement.querySelector(".modal__preview-caption");
   }
 
+  //  defines an open method for the PopupWithImage class.
   open(name, link) {
-    // Set the caption's text content to name
-    this._caption.textContent = name;
-
-    // Set the image's source to link
-    this._image.src = link;
-
-    // Set the image's alt attribute to name
-    this._image.alt = name;
-
-    // Call the open method of the superclass (Popup)
+    // sets the text content of the .modal__preview-caption element to the name parameter.
+    this._previewModalCaption.textContent = name;
+    // sets the src attribute of the .modal__preview-image element to the link parameter.
+    this._previewModalImage.src = link;
+    // sets the alt attribute of the .modal__preview-image element to the name parameter.
+    this._previewModalImage.alt = name;
+    // calls the parent Popup class's open method.
     super.open();
   }
 }
