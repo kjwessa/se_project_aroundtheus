1e5259152aff358da8f3d14f6cf2ab64b1a57d6c
Kevin Wessa <kjwessa@gmail.com>
2023-05-10 10:22:25 -0500

    feat: build initial Api class without avatar functionality

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,32 +4,83 @@ export default class Api {
     this._headers = headers;
   }
 
+  //* Handle the response from the server
   //TODO Add in the comments for the function below
-  _handleResposne(res) {}
+  _handleResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
 
   //TODO Add in the comments for the function below
-  getUserInfo() {}
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    }).then(this._handleResponse);
+  }
+
+  //* Get the initial cards from the server
+  //TODO Add in the comments for the function below
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "GET",
+      headers: this._headers,
+    }).then(this._handleResponse);
+  }
+
+  //* Promise All to get the initial user info and cards
+  //TODO Add in the comments for the function below
+  getAPIInfo() {
+    return Promise.all([this.getUserInfo(), this.getInitialCards()]);
+  }
+
+  //* Update the user profile info
+  //TODO Add in the comments for the function below
+  updateProfileInfo(userData) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userData.name,
+        about: userData.about,
+      }),
+    }).then(this._handleResponse);
+  }
 
   //TODO Add in the comments for the function below
-  getInitialCards() {}
+  addNewCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({ name, link }),
+    }).then(this._handleResponse);
+  }
 
   //TODO Add in the comments for the function below
-  getAppInfo() {}
+  deleteUserCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handleResponse);
+  }
 
   //TODO Add in the comments for the function below
-  updateProfileInfo() {}
+  addCardLikes(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._handleResponse);
+  }
 
   //TODO Add in the comments for the function below
-  addNewCard() {}
-
-  //TODO Add in the comments for the function below
-  deleteUserCard() {}
-
-  //TODO Add in the comments for the function below
-  addCardLikes() {}
-
-  //TODO Add in the comments for the function below
-  removeCardLikes() {}
+  removeCardLikes(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handleResponse);
+  }
 
   //TODO Build the function below and add in the comments
   updateProfileAvatar() {}
